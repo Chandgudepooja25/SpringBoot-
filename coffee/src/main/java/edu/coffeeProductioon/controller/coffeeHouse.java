8cbe5b246dee08586b724518abe6da22525be7b0
Chandgudepooja25 <121222757+Chandgudepooja25@users.noreply.github.com>
2023-03-06 15:21:21 +0530

    Update coffeeHouse.java

--- coffee/src/main/java/edu/coffeeProductioon/controller/coffeeHouse.java
+++ coffee/src/main/java/edu/coffeeProductioon/controller/coffeeHouse.java
@@ -15,11 +15,12 @@ public class coffeeHouse {
 	private List<Coffee> coffee = new ArrayList<>();
 	public coffeeHouse() 
 	{
+                 // Add coffee to the list
 		coffee.addAll(List.of(new Coffee(250, "Macchiato"), new Coffee(300, "Cortado"), new Coffee(450, "Americano"),
 				new Coffee(550, "Latte"), new Coffee(350, "Instant Coffee"), new Coffee(300, "Flat White")));
 		
 	}
-	
+	//To display all the coffee to the list
 	@RequestMapping(value = "/coffee", method = RequestMethod.GET)
 	Iterable<Coffee> getAllLeds()
 	{
